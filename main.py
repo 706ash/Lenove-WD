9cec430f323670ea1fa97cefc74b7969460d652b
706ash <carameloco7@gmail.com>
2025-02-10 11:57:52 +0530

    .

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/hi")
+async def hi():
+    return {"message": "Hello World"}
+
+@app.get("/bye")
+async def bye():
+    return {"message": "Bye World"}
+
+
